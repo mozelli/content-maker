b2fe88c01a05f7cc696e421453bedcf32aff0724
João Mozelli Neto <joaomozelli@hotmail.com>
2023-03-17 13:40:29 -0300

    feat: create input robot

--- src/robots/user-input.js
+++
@@ -1,8 +0,0 @@
-const readline = require('readline-sync');
-
-function robot(content) {
-  content.searchTerm = readline.question("Informe um termo de busca: ");
-  content.url = `https://pt.wikipedia.org/wiki/${content.searchTerm}`;
-}
-
-module.exports = robot;
